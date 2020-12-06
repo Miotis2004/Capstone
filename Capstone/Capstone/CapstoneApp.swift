2b1a19697072dffea890dce170409aedd64e312f
Miotis2004 <miotis2004@gmail.com>
2020-12-06 17:34:49 -0500

    first commit

---
+++ Capstone/Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Ronald Joubert on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
